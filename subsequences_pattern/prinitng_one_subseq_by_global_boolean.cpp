e0c97006957528bf17b2b1cb0ce42f20b68643ca
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-07-20 09:42:33 +0530

    Add function to print one subsequence with sum k

---
+++ subsequences_pattern/prinitng_one_subseq_by_global_boolean.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool flag=false;
+void printSubSum(int idx,vector<int>&ds,int sum,int k ,int n , vector<int>arr){
+    if(idx==n){
+        if(sum==k && flag==false){
+            flag=true;
+            for(auto it:ds) cout<<it<<" ";
+            cout<<endl;
+        }
+        return;
+    }
+    ds.push_back(arr[idx]);
+    sum+=arr[idx];
+    printSubSum(idx+1,ds,sum,k,n,arr);
+
+    ds.pop_back();
+    sum-=arr[idx];
+    printSubSum(idx+1,ds,sum,k,n,arr);
+}
+
+int main(){
+    vector<int>arr={1,2,3,4};
+    int n = arr.size();
+    vector<int>ds;
+    int k = 3;
+    printSubSum(0,ds,0,3,n,arr);
+    return 0;
+}
